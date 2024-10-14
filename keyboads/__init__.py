db42dc05a50538ced9805cabc6d758eb0d919f17
Fedorov Semen <FedorovSemen28@yandex.ru>
2024-10-14 16:57:16 +0300

    connet router to upper router

---
+++ keyboads/__init__.py
@@ -0,0 +1,13 @@
+__all__ = (
+    "get_confirm_button",
+    "get_keyboard",
+    "get_start_keyboard",
+    "get_support_button",
+)
+
+from .common_keyboard import (
+    get_confirm_button,
+    get_keyboard,
+    get_start_keyboard,
+    get_support_button,
+)
