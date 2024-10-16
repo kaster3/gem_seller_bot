0875956bdd9bdd7e7b129851681fbc675caa1bed
Fedorov Semen <FedorovSemen28@yandex.ru>
2024-10-16 11:03:57 +0300

    add cancel_handler

--- routers/commands/base_commands.py
+++ routers/commands/base_commands.py
@@ -1,5 +1,6 @@
 from aiogram import F, Router, types
 from aiogram.filters import Command, CommandStart
+from aiogram.fsm.context import FSMContext
 
 from keyboads import get_start_keyboard
 
@@ -7,7 +8,6 @@ router = Router(name=__name__)
 
 
 @router.message(CommandStart())
-@router.message(F.text == "🚫 Отмена 🚫")
 async def start_handler(message: types.Message) -> None:
     await message.answer(
         text=f"Привет, {message.from_user.first_name}! 👋",
@@ -20,3 +20,13 @@ async def help_handler(message: types.Message) -> None:
     await message.answer(
         text="Ты можешь приобрести у меня гемы для TDD по низким ценам",
     )
+
+
+@router.message(F.text == "🚫 Отмена 🚫")
+async def cancel_handler(message: types.Message, state: FSMContext) -> None:
+    await message.answer(
+        text="Ты снова в главном меню!",
+        reply_markup=get_start_keyboard(),
+    )
+    if state:
+        await state.clear()
